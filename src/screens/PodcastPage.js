3b3490f0946412f5942724841007a12f20bd79bd
ismozi <61505183+ismozi@users.noreply.github.com>
2021-12-31 02:45:01 -0500

    Debut page dun podcast

--- src/screens/PodcastPage.js
+++ src/screens/PodcastPage.js
@@ -3,8 +3,13 @@ const PodcastPage = (props) => {
         <div className="homeScreen">
             <div id="podcastPageItem">
                 <img id="podcastPageImg" src="https://i.imgur.com/F8pthmM.png"></img>
-                <h1 id="podcastPageTitle">Mike Ward Sous Ecoute</h1>
+                <div id="podcastPageArticle">
+                    <h1 id="podcastPageTitle">Mike Ward Sous Ecoute</h1>
+                    <p id="podcastPageText">Chaque semaines l'humoriste Mike Ward reçoit des personalités du monde du divertissement pour des entrevues et discussions candides.</p>
+                    <div id="socialMediaPanel">Share</div>
+                </div>
             </div>  
+            <hr class="solid"/>
         </div>
       );
 }
