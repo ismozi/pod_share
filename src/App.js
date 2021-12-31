1ccd490d944d75ca36d9db42758cdfa64a8fdeee
ismozi <61505183+ismozi@users.noreply.github.com>
2021-12-31 00:44:54 -0500

    avancement recherche de podcast

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import ComponentTest from "./components/ComponentTest.js"
 import HomeScreen from "./screens/HomeScreen.js"
 import Header from "./components/Header.js"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom" ;
+import PodcastPage from "./screens/PodcastPage.js";
 
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route path = "/" element={<HomeScreen/>}></Route>
+        <Route exact path = "/" element={<HomeScreen/>}></Route>
         <Route path = "/discover" element={<ComponentTest/>}></Route>
+        <Route path = "/podcast/" element={<PodcastPage/>}></Route>
       </Routes>
     </Router>
   );
---
+++ src/screens/PodcastPage.js
@@ -0,0 +1,12 @@
+const PodcastPage = (props) => {
+    return (
+        <div className="homeScreen">
+            <div id="podcastPageItem">
+                <img id="podcastPageImg" src="https://i.imgur.com/F8pthmM.png"></img>
+                <h1 id="podcastPageTitle">Mike Ward Sous Ecoute</h1>
+            </div>  
+        </div>
+      );
+}
+
+export default PodcastPage;
